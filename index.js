6e92fc224534c500a06eb935499c84ae0bde5318
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-02-08 21:19:08 -0500

    adding DB info

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 
-const { getAllStations, getBySlug  }= require('./controllers')
+// const { getAllStations, getBySlug  }= require('./controllers')
 
 const app = express()
-
+/*
 app.get('/', getAllStations)
 app.get('/:slug', getBySlug)
-app.all('*',erorMsg)
+
+*/
+//app.all('*',erorMsg)
 
 app.listen(1337, () => {
     console.log('Listening on port 1337....') // eslint-disable-line no-console
-
 })
