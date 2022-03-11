7ca7abaa73d09536ed5b9075b74cd45e1c0e7072
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-03-10 20:03:47 -0500

    connection problem selved

--- client/src/components/Radio.js
+++ client/src/components/Radio.js
@@ -4,27 +4,25 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Title from './Title'
 import Station from './Station'
-
 import axios from 'axios';
 const Radio = () => {
 const [callLetters, setCallLetters] = useState('')
 const [allStations, setAllStations] = useState([])
-
 useEffect(() => {
     console.log('My useEffect is working')
     const fetchAllStations = async () => {
-        const fetchedData = await axios.get('http://localhost:1337/api')
-
+        const fetchedData = await axios.get('http://localhost:1337/api/stations')
         setAllStations(fetchedData.data)
         console.log(fetchedData.data)
     }
     fetchAllStations()
 }, [callLetters])
-
+console.log(allStations)
+console.log(allStations.map((x) => x.callLetters))
+console.log(allStations.map((x) => x.frequency))
 const handleCallLetters = event => {
 setCallLetters(event.target.value)
 }
-
     return (
         <div>
         <Title callLetters = {callLetters}/>
@@ -33,5 +31,4 @@ setCallLetters(event.target.value)
         </div>
     )
 }
-
 export default Radio;
