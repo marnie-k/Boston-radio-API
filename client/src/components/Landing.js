f3fd79c598ec58defd3dfa10b9ff6d0775458acb
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-03-12 18:58:47 -0500

    trying to get useState and useEffect working

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,11 +1,30 @@
-import react from  'react';
-import AllStations from  './AllStations';
+import axios from 'axios';
+import react, { useState,useEffect } from  'react';
+// import AllStations from  './AllStations';
 
 const Landing = () => {
+
+  //POS named stations to hold all stations
+  const [stations, setStations] = useState(['hi'])
+
+  useEffect(() => {
+    const fetchStations = async() => {
+      const fetchedData = await axios.get('http://localhost:1337/api/stations') 
+      setStations(fetchedData.data)
+      console.log(fetchedData.data)
+    }
+  fetchStations()  
+  },[])
+
   return (
     <div>
       <h1 className= 'Title'>Welcome to Boston Radio</h1>
       <h2 className= 'Header'>Greater Boston area radio stations</h2>
+      <div>{stations.map(station=>{
+        return(
+          <div>{station.id}</div>
+        )
+      })}</div>
     </div>
   );
 };
